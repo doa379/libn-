da7497fd9e49fd5ce3ce24ad1051542da5af198c
doa379 <doa379@gmail.com>
2020-01-30 00:43:10 +0000

    ;

--- nn.cpp
+++ nn.cpp
@@ -6,12 +6,12 @@
 
 Nn::Nn(Spec *s)
 {
-  this->s = Spec(*s);
-  init_weight(&w);
-  init_weight(&g, 0);
-  g0 = Weight(g);
-  prev_g = Weight(g);
-  init_weight(&prev_d, INIT_D);
+	this->s = Spec(*s);
+	init_weight(&w);
+	init_weight(&g, 0);
+	g0 = Weight(g);
+	prev_g = Weight(g);
+	init_weight(&prev_d, INIT_D);
 }
 
 Nn::~Nn(void)
@@ -21,289 +21,314 @@ Nn::~Nn(void)
 
 void Nn::init_weight(Weight *u)
 {
-  std::default_random_engine generator;
-  std::uniform_int_distribution<int> distribution(1, KILO - 1);
+	std::default_random_engine generator;
+	std::uniform_int_distribution<int> distribution(1, KILO - 1);
 
-  for (unsigned i = 0; i < s.NI; i++)
-    {
-      std::vector<double> IHW;
-      
-      for (unsigned j = 0; j < s.NH; j++)
-	IHW.emplace_back((double) distribution(generator) / KILO);
+	for (unsigned i = 0; i < s.NI; i++)
+	{
+		std::vector<double> IHW;
 
-      u->IHW.emplace_back(IHW);
-    }
+		for (unsigned j = 0; j < s.NH; j++)
+			IHW.emplace_back((double) distribution(generator) / KILO);
 
-  for (unsigned i = 0; i < s.NH; i++)
-    u->HB.emplace_back((double) distribution(generator) / KILO);
+		u->IHW.emplace_back(IHW);
+	}
 
-  for (unsigned i = 0; i < s.NO; i++)
-    {
-      std::vector<double> HOW;
-      
-      for (unsigned j = 0; j < s.NH; j++)
-	HOW.emplace_back((double) distribution(generator) / KILO);
+	for (unsigned i = 0; i < s.NH; i++)
+		u->HB.emplace_back((double) distribution(generator) / KILO);
 
-      u->HOW.emplace_back(HOW);
-    }
-  
-  for (unsigned i = 0; i < s.NO; i++)
-    u->OB.emplace_back((double) distribution(generator) / KILO);
+	for (unsigned i = 0; i < s.NO; i++)
+	{
+		std::vector<double> HOW;
+
+		for (unsigned j = 0; j < s.NH; j++)
+			HOW.emplace_back((double) distribution(generator) / KILO);
+
+		u->HOW.emplace_back(HOW);
+	}
+
+	for (unsigned i = 0; i < s.NO; i++)
+		u->OB.emplace_back((double) distribution(generator) / KILO);
 }
 
 void Nn::init_weight(Weight *u, double v)
 {
-  for (unsigned i = 0; i < s.NI; i++)
-    {
-      std::vector<double> IHW;
-      IHW.assign(s.NH, v);
-      u->IHW.emplace_back(IHW);
-    }
+	for (unsigned i = 0; i < s.NI; i++)
+	{
+		std::vector<double> IHW;
+		IHW.assign(s.NH, v);
+		u->IHW.emplace_back(IHW);
+	}
 
-  u->HB.assign(s.NH, v);
+	u->HB.assign(s.NH, v);
 
-  for (unsigned i = 0; i < s.NO; i++)
-    {
-      std::vector<double> HOW;
-      HOW.assign(s.NH, v);
-      u->HOW.emplace_back(HOW);
-    }
+	for (unsigned i = 0; i < s.NO; i++)
+	{
+		std::vector<double> HOW;
+		HOW.assign(s.NH, v);
+		u->HOW.emplace_back(HOW);
+	}
 
-  u->OB.assign(s.NO, v);
+	u->OB.assign(s.NO, v);
 }
 
 void Nn::normalize(std::vector<std::vector<double>> *O, std::vector<std::vector<double>> *I)
 {
-  std::vector<std::vector<double>> RT, NT; // Raw/Normalized Transpose
-  transpose(&RT, I);
-  
-  for (std::vector<double> &R : RT)
-    {
-      double min = *std::min_element(R.begin(), R.end()),
-	max = *std::max_element(R.begin(), R.end());
-      std::vector<double> NR;
+	std::vector<std::vector<double>> RT, NT; // Raw/Normalized Transpose
+	transpose(&RT, I);
 
-      for (std::vector<double>::iterator r = R.begin(); r < R.end() - 1; r++)
+	for (std::vector<double> &R : RT)
 	{
-	  double n = perc_diff(*r, *(r + 1), min, max) / 100;
-	  NR.emplace_back(n);
+		double min = *std::min_element(R.begin(), R.end()),
+					 max = *std::max_element(R.begin(), R.end());
+		std::vector<double> NR;
+
+		for (std::vector<double>::iterator r = R.begin(); r < R.end() - 1; r++)
+		{
+			double n = perc_ch(*r, *(r + 1), min, max) / 100;
+			NR.emplace_back(n);
+		}
+
+		NT.emplace_back(NR);
 	}
 
-      NT.emplace_back(NR);
-    }
-
-  transpose(O, &NT);
+	transpose(O, &NT);
 }
 
 std::vector<double> Nn::train(std::vector<std::vector<double>> *I, size_t epochs)
 {
-  std::vector<double> MSE;
-  
-  for (size_t i = 0; i < epochs; i++)
-    {
-      if ((i + 1) % (epochs / 10) == 0)
-	MSE.emplace_back(mse(I));
+	std::vector<double> MSE;
 
-      g = Weight(g0);
-      calc_grads(I);
-      update_w();
-    }
+	for (size_t i = 0; i < epochs; i++)
+	{
+		if ((i + 1) % (epochs / 10) == 0)
+			MSE.emplace_back(mse(I));
 
-  return MSE;
+		g = Weight(g0);
+		calc_grads(I);
+		update_w();
+	}
+
+	return MSE;
 }
 
 void Nn::calc_grads(std::vector<std::vector<double>> *I)
 {
-  double OG[s.NO], HG[s.NH];
-  
-  for (std::vector<std::vector<double>>::iterator J = I->begin(); J < I->end() - 1; J++)
-    {
-      std::vector<double> P = calc_outputs(&(*J)),
-	T = *(J + 1);
+	double OG[s.NO], HG[s.NH];
 
-      for (unsigned i = 0; i < s.NO; i++)
-	OG[i] = dsoftmax(P[i]) * (P[i] - T[i]);
-
-      for (unsigned i = 0; i < s.NH; i++)
+	for (std::vector<std::vector<double>>::iterator J = I->begin(); J < I->end() - 1; J++)
 	{
-	  double sum = 0;
+		std::vector<double> P = calc_outputs(&(*J)),
+			T = *(J + 1);
 
-	  for (unsigned j = 0; j < s.NO; j++)
-	    sum += OG[j] * w.HOW[j][i];
+		for (unsigned i = 0; i < s.NO; i++)
+			//OG[i] = dsoftmax(P[i]) * (P[i] - T[i]);
+			OG[i] = dtanh(P[i]) * (P[i] - T[i]);
 
-	  HG[i] = dactivation(HO[i]) * sum;
+		for (unsigned i = 0; i < s.NH; i++)
+		{
+			double sum = 0;
+
+			for (unsigned j = 0; j < s.NO; j++)
+				sum += OG[j] * w.HOW[j][i];
+
+			HG[i] = dtanh(HO[i]) * sum;
+		}
+
+		for (unsigned i = 0; i < s.NH; i++)
+			for (unsigned j = 0; j < s.NO; j++)
+				g.HOW[j][i] += OG[j] * HO[i];
+
+		for (unsigned i = 0; i < s.NO; i++)
+			g.OB[i] += OG[i];
+
+		for (unsigned i = 0; i < s.NI; i++)
+			for (unsigned j = 0; j < s.NH; j++)
+				g.IHW[i][j] += HG[j] * (*J)[i];
+
+		for (unsigned i = 0; i < s.NH; i++)
+			g.HB[i] += HG[i];
 	}
-
-      for (unsigned i = 0; i < s.NH; i++)
-	for (unsigned j = 0; j < s.NO; j++)
-	  g.HOW[j][i] += OG[j] * HO[i];
-
-      for (unsigned i = 0; i < s.NO; i++)
-	g.OB[i] += OG[i];
-
-      for (unsigned i = 0; i < s.NI; i++)
-	for (unsigned j = 0; j < s.NH; j++)
-	  g.IHW[i][j] += HG[j] * (*J)[i];
-
-      for (unsigned i = 0; i < s.NH; i++)
-	g.HB[i] += HG[i];
-    }
-}
-
-void Nn::update_w(void)
-{
-  for (unsigned i = 0; i < s.NI; i++)
-    for (unsigned j = 0; j < s.NH; j++)
-      rprop(&w.IHW[i][j], &prev_g.IHW[i][j], &g.IHW[i][j], &prev_d.IHW[i][j]);
-
-  for (unsigned i = 0; i < s.NH; i++)
-    rprop(&w.HB[i], &prev_g.HB[i], &g.HB[i], &prev_d.HB[i]);
-  
-  for (unsigned i = 0; i < s.NH; i++)
-    for (unsigned j = 0; j < s.NO; j++)
-      rprop(&w.HOW[j][i], &prev_g.HOW[j][i], &g.HOW[j][i], &prev_d.HOW[j][i]);
-
-  for (unsigned i = 0; i < s.NO; i++)
-    rprop(&w.OB[i], &prev_g.OB[i], &g.OB[i], &prev_d.OB[i]);
-}
-
-void Nn::rprop(double *w, double *prev_g, double *g, double *prev_d)
-{
-  double d;
-  
-  if (*prev_g * *g > 0)
-    {
-      d = *prev_d * s.eta_p;
-	
-      if (d > s.delta_max)
-	d = s.delta_max;
-
-      *w += -sign(*g) * d;
-    }
-
-  else if (*prev_g * *g < 0)
-    {
-      d = *prev_d * s.eta_m;
-
-      if (d < s.delta_min)
-	d = s.delta_min;
-
-      *w -= *prev_d;
-      *g = 0;
-    }
-
-  else
-    {
-      d = *prev_d;
-      
-      if (d > s.delta_max)
-	d = s.delta_max;
-
-      else if (d < s.delta_min)
-	d = s.delta_min;
-	   
-      *w += -sign(*g) * d;
-    }
-
-  *prev_d = d;
-  *prev_g = *g;
-}
-
-double Nn::mse(std::vector<std::vector<double>> *I)
-{
-  double se = 0;
-  
-  for (std::vector<std::vector<double>>::iterator J = I->begin(); J < I->end() - 1; J++)
-    {
-      std::vector<double> P = calc_outputs(&(*J));
-
-      for (unsigned i = 0; i < s.NO; i++)
-	{
-	  double err = (*(J + 1))[i] - P[i];
-	  se += err * err;
-	}
-    }
-  
-  return se / (I->size() - 1) / s.NO;
 }
 
 std::vector<double> Nn::calc_outputs(std::vector<double> *I)
 { // Online Input
-  std::vector<double> O{ };
-  HO.clear();
+	std::vector<double> O{ };
+	HO.clear();
 
-  for (unsigned i = 0; i < s.NH; i++)
-    {
-      double hs = 0;
-	
-      for (unsigned j = 0; j < s.NI; j++)
-	hs += (*I)[j] * w.IHW[j][i] + w.HB[i];
+	for (unsigned i = 0; i < s.NH; i++)
+	{
+		double hs = 0;
 
-      HO.emplace_back(activation(hs));
-    }
+		for (unsigned j = 0; j < s.NI; j++)
+			hs += (*I)[j] * w.IHW[j][i] + w.HB[i];
 
-  for (unsigned i = 0; i < s.NO; i++)
-    {
-      double os = 0;
-      
-      for (unsigned j = 0; j < s.NH; j++)
-	os += HO[j] * w.HOW[i][j] + w.OB[i];
+		HO.emplace_back(dtanh(hs));
+	}
 
-      O.emplace_back(os);
-    }
+	for (unsigned i = 0; i < s.NO; i++)
+	{
+		double os = 0;
 
-  //return softmax(&O);
-  return O;
+		for (unsigned j = 0; j < s.NH; j++)
+			os += HO[j] * w.HOW[i][j] + w.OB[i];
+
+		O.emplace_back(os);
+	}
+
+	//return softmax(&O);
+	return O;
+}
+
+void Nn::update_w(void)
+{
+	for (unsigned i = 0; i < s.NI; i++)
+		for (unsigned j = 0; j < s.NH; j++)
+			rprop(&w.IHW[i][j], &prev_g.IHW[i][j], &g.IHW[i][j], &prev_d.IHW[i][j]);
+
+	for (unsigned i = 0; i < s.NH; i++)
+		rprop(&w.HB[i], &prev_g.HB[i], &g.HB[i], &prev_d.HB[i]);
+
+	for (unsigned i = 0; i < s.NH; i++)
+		for (unsigned j = 0; j < s.NO; j++)
+			rprop(&w.HOW[j][i], &prev_g.HOW[j][i], &g.HOW[j][i], &prev_d.HOW[j][i]);
+
+	for (unsigned i = 0; i < s.NO; i++)
+		rprop(&w.OB[i], &prev_g.OB[i], &g.OB[i], &prev_d.OB[i]);
+}
+
+void Nn::rprop(double *w, double *prev_g, double *g, double *prev_d)
+{
+	double d;
+
+	if (*prev_g * *g > 0)
+	{
+		d = *prev_d * s.eta_p;
+
+		if (d > s.delta_max)
+			d = s.delta_max;
+
+		else if (d < s.delta_min)
+			d = s.delta_min;
+
+		*w += -sign(*g) * d;
+	}
+
+	else if (*prev_g * *g < 0)
+	{
+		d = *prev_d * s.eta_m;
+
+		if (d < s.delta_min)
+			d = s.delta_min;
+
+		else if (d < s.delta_min)
+			d = s.delta_min;
+
+		*w -= *prev_d;
+		*g = 0;
+	}
+
+	else
+	{
+		d = *prev_d;
+
+		if (d > s.delta_max)
+			d = s.delta_max;
+
+		else if (d < s.delta_min)
+			d = s.delta_min;
+
+		*w += -sign(*g) * d;
+	}
+
+	*prev_d = d;
+	*prev_g = *g;
+}
+
+double Nn::mse(std::vector<std::vector<double>> *I)
+{
+	double se = 0;
+
+	for (std::vector<std::vector<double>>::iterator J = I->begin(); J < I->end() - 1; J++)
+	{
+		std::vector<double> P = calc_outputs(&(*J));
+
+		for (unsigned i = 0; i < s.NO; i++)
+		{
+			double err = (*(J + 1))[i] - P[i];
+			se += err * err;
+		}
+	}
+
+	return se / (I->size() - 1) / s.NO;
 }
 
 std::vector<double> Nn::softmax(std::vector<double> *I)
 {
-  double max = *std::max_element(I->begin(), I->end()),
-    scale = 0;
+	double max = *std::max_element(I->begin(), I->end()),
+				 scale = 0;
 
-  for (std::vector<double>::iterator i = I->begin(); i < I->end(); i++)
-    scale += exp(*i - max);
+	for (std::vector<double>::iterator i = I->begin(); i < I->end(); i++)
+		scale += exp(*i - max);
 
-  std::vector<double> O;
+	std::vector<double> O;
 
-  for (std::vector<double>::iterator i = I->begin(); i < I->end(); i++)
-    {
-      double res = exp(*i - max) / scale;
-      O.emplace_back(res);
-    }
+	for (std::vector<double>::iterator i = I->begin(); i < I->end(); i++)
+	{
+		double res = exp(*i - max) / scale;
+		O.emplace_back(res);
+	}
 
-  return O;
+	return O;
 }
 
 char Nn::sign(double a)
 {
-  if (fabs(a) < NANO)
-    return 0;
+	if (fabs(a) < NANO)
+		return 0;
 
-  else if (a > 0)
-    return 1;
+	else if (a > 0)
+		return 1;
 
-  return -1;
+	return -1;
 }
 
 double Nn::norm(double p, double min, double max)
 {
-  if (min == max)
-    return 0;
-  
-  return (p - min) / (max - min);
+	if (min == max)
+		return 0;
+
+	return (p - min) / (max - min);
 }
 
 double Nn::perc_diff(double a, double b, double min, double max)
 {
+	a = norm(a, min, max);
+	b = norm(b, min, max);
+
+	if (!sign(a) && !sign(b))
+		return 0;
+
+	else if (!sign(a))
+		return 100;
+
+	return fabs(b - a) / (a + b) * 2 * 100;
+}
+
+double Nn::perc_ch(double a, double b, double min, double max)
+{
   a = norm(a, min, max);
   b = norm(b, min, max);
-  
+
   if (!sign(a) && !sign(b))
     return 0;
+  
+  else if (!sign(a) && sign(b) < 0)
+    return -100;
 
-  else if (!sign(a))
-    return 100;
+  else if (!sign(a) && sign(b) > 0)
+      return 100;
 
-  return fabs(b - a) / (a + b) * 2 * 100;
+  return (b - a) / fabs(a) * 100;
 }
+
--- test_nn.cpp
+++ test_nn.cpp
@@ -11,74 +11,75 @@
 
 void print_vector(std::vector<double> *I)
 {
-  for (double &d : *I)
-    std::cout << d << " ";
+	for (double &d : *I)
+		std::cout << d << " ";
 
-  std::cout << "\n";
+	std::cout << "\n";
 }
 
 void print_matrix(std::vector<std::vector<double>> *I)
 {
-  size_t r = I->size(),
-    c = I->at(0).size();
+	size_t r = I->size(),
+				 c = I->at(0).size();
 
-  for (size_t i = 0; i < r; i++)
-  {
-    for (size_t j = 0; j < c; j++)
-      std::cout << (*I)[i][j] << " ";
+	for (size_t i = 0; i < r; i++)
+	{
+		for (size_t j = 0; j < c; j++)
+			std::cout << (*I)[i][j] << " ";
 
-    std::cout << "\n";
-  }
+		std::cout << "\n";
+	}
 }
 
 void make_matrix(std::vector<std::vector<double>> *O, unsigned r, unsigned c)
 {
-  for (unsigned i = 0; i < r; i++)
-    {
-      std::vector<double> C;
+	for (unsigned i = 0; i < r; i++)
+	{
+		std::vector<double> C;
 
-      for (unsigned j = 0; j < c; j++)
-	C.emplace_back((double) j);
+		for (unsigned j = 0; j < c; j++)
+			C.emplace_back((double) j);
 
-      O->emplace_back(C);
-    }
+		O->emplace_back(C);
+	}
 }
 
 void make_matrix_random(std::vector<std::vector<double>> *O, unsigned r, unsigned c)
 {
-  std::default_random_engine generator;
-  std::uniform_int_distribution<int> distribution(1, DECA - 1);
-  
-  for (unsigned i = 0; i < r; i++)
-    {
-      std::vector<double> C;
+	std::default_random_engine generator;
+	std::uniform_int_distribution<int> distribution(1, DECA - 1);
 
-      for (unsigned j = 0; j < c; j++)
-	C.emplace_back((double) distribution(generator) / DECA);
+	for (unsigned i = 0; i < r; i++)
+	{
+		std::vector<double> C;
 
-      O->emplace_back(C);
-    }
+		for (unsigned j = 0; j < c; j++)
+			C.emplace_back((double) distribution(generator) / DECA);
+
+		O->emplace_back(C);
+	}
 }
 
 int main()
 {
-  Spec s{ 1, NF, NH, NO, 1.2, 0.5, 50.0, MICRO };
-  Nn nn(&s);
-  std::vector<std::vector<double>> N, I;
-  make_matrix_random(&I, 10, 5);
-  std::cout << "Original Data\n======\n";
-  print_matrix(&I);
-  nn.normalize(&N, &I);
-  std::cout << "Normalized Data\n======\n";
-  print_matrix(&N);
-  std::vector<double> MSE = nn.train(&N, 1000);
-  std::cout << "Errors\n======\n";
-  
-  for (double &mse : MSE)
-    std::cout << mse << std::endl;
+	Spec s{ NF, NH, NO, 1.2, 0.5, 50.0, MICRO };
+	Nn nn(&s);
+	std::vector<std::vector<double>> N, I;
+	make_matrix(&I, 10, 5);
+	std::cout << "Original Data\n======\n";
+	print_matrix(&I);
+	std::cout << "Normalized Data\n======\n";
+	nn.normalize(&N, &I);
+//N = I;
+	print_matrix(&N);
+	std::vector<double> MSE = nn.train(&N, 1000);
+	std::cout << "Errors\n======\n";
 
-  std::cout << "Output\n======\n";
-  std::vector<double> O = nn.calc_outputs(&N.back());
-  print_vector(&O);
-  return 0;
+	for (double &mse : MSE)
+		std::cout << mse << std::endl;
+
+	std::cout << "Output\n======\n";
+	std::vector<double> O = nn.calc_outputs(&N.back());
+	print_vector(&O);
+	return 0;
 }
